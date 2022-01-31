4a64a0b6dc005d5b5080207b6ab6ed2a3360ede6
Liande Guo <47792949+LyrikTasche@users.noreply.github.com>
2022-01-31 22:43:51 +0100

    Create File
    The Software now generates a file with the URLs,
    so VSC isn´t necessary anymore

--- generatorappmybox.py
+++ generatorappmybox.py
@@ -1,18 +1,22 @@
 from fileinput import filename
+from mailbox import linesep
 import random
 from timeit import repeat
-
-filename = "generated"
-myfile = open(filename, "w")
+import sys
 
 url = "http/www.appmybox.com/mobile/?4102&00194"
-
+nl = "\n"
 # Change the number after range to set the number off links to be generated
 for i in range(10):
     first = chr(random.randint(ord("A"), ord("Z")))
     second = chr(random.randint(ord("A"), ord("Z")))
     third = chr(random.randint(ord("A"), ord("Z")))
     thourd = chr(random.randint(ord("A"), ord("Z")))
-    print(url+first+second+third+thourd)
-
+    urlo = url+first+second+third+thourd
+    urlt = urlo, nl
+    # Print URLs into file
+    with open("urls.txt", "a") as f:
+        f.writelines(urlt)
 # I would recommend to use the website qrbatch.com to generate the QR-Codes
+
+# Made by Liande Guo aka LyrikTasche
